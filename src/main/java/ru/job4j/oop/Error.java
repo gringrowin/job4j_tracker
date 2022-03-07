cb477963b627fe932dadab8b357e0c3b3e0a25e1
wertifag <gringrowin@gmail.com>
2022-03-07 17:45:22 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void infoError() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error firstError = new Error();
+        Error secondError = new Error(true, 1, "Караул!");
+        Error thirdError = new Error(false, 6, "Не критичная!");
+        firstError.infoError();
+        secondError.infoError();
+        thirdError.infoError();
+    }
+}
