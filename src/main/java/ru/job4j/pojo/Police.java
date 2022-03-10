b7a119813e41da85e518a872ac009c0f8552902e
wertifag <gringrowin@gmail.com>
2022-03-10 19:01:13 +0300

    урок 1.2.3

---
+++ src/main/java/ru/job4j/pojo/Police.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Police {
+    public static void main(String[] args) {
+        License license = new License();
+        license.setOwner("Max Pavlishin");
+        license.setModel("Nissan");
+        license.setCode("oo111o");
+        license.setCreated(new Date());
+
+        System.out.println(license.getOwner() + " has a car - " + license.getModel() + " : " + license.getCode());
+    }
+}
