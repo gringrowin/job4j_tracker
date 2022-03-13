20bee345d2cc11110b2523304023d82e5f4ca596
wertifag <gringrowin@gmail.com>
2022-03-13 08:58:48 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,8 +6,6 @@ public class StartUI {
 
     public static void main(String[] args) {
     Item item = new Item("Объект");
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-    String itemCreated = item.getCreated().format(formatter);
-    System.out.println(item.getName() + " создан " + itemCreated);
+    System.out.println(item);
     }
 }
