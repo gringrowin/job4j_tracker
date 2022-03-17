443ddfaf2996279fec92490338ad0ed7ee5cd20d
wertifag <gringrowin@gmail.com>
2022-03-17 08:26:20 +0300

    8. Реализация меню за счет шаблона стратегия. [#181780]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,19 +2,6 @@ package ru.job4j.tracker;
 
 public class StartUI {
 
-    public static void findItemByName(Input input, Tracker tracker) {
-        System.out.println("=== Find items by name ===");
-        String name = input.askStr("Enter name: ");
-        Item[] items = tracker.findByName(name);
-        if (items.length > 0) {
-            for (Item item : items) {
-                System.out.println(item);
-            }
-        } else {
-            System.out.println("Заявки с именем: " + name + " не найдены.");
-        }
-    }
-
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
         while (run) {
