4371de477c5d42cab424951bc9dd6143a22e615d
wertifag <gringrowin@gmail.com>
2022-03-25 10:01:34 +0300

    2. Отсортировать департаменты [#34352 #279843]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,7 +8,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] o1Split = o1.split("/");
         String[] o2Split = o2.split("/");
-        int rsl = o1Split[0].compareTo(o2Split[0]);
-        return rsl == 0 ? o1.compareTo(o2) : o2.compareTo(o1);
+        int rsl = o2Split[0].compareTo(o1Split[0]);
+        return rsl == 0 ? o1.compareTo(o2) : rsl;
     }
 }
