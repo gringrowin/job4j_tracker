9921ebd737035c2c64b242076092affc113af461
wertifag <gringrowin@gmail.com>
2022-03-28 15:02:17 +0300

    1. Фильтрация учеников [#110058]

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -38,7 +38,8 @@ public class SchoolTest {
                 new Student(80, "Surname8")
         );
         School sc = new School();
-        Predicate<Student> pr = student -> student.getScore() >= 50 && student.getScore() < 70;
+        Predicate<Student> pr = student -> student.getScore() >= 50
+                && student.getScore() < 70;
         List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(50, "Surname5"));
